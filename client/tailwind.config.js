d092115a8b4c0e845ffec192556b95d4db2296e2
Milan Jovanović <milannis.995@gmail.com>
2020-10-25 11:50:10 +0100

    - Integrated Tailwind CSS into the Angular project

---
+++ client/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+    standardFontWeights: true,
+  },
+  purge: ['./src/**/*.html', './src/**/*.ts'],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
