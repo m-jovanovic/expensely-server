961f3bd8cbc5ad8d47bb92e22792c5a6ab091e79
Milan Jovanović <milannis.995@gmail.com>
2020-11-05 17:07:45 +0100

    - Added mock layout to dashboard

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,12 +2,20 @@ module.exports = {
   future: {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
-    standardFontWeights: true,
+    standardFontWeights: true
   },
   purge: ['./src/**/*.html', './src/**/*.ts'],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        72: '18rem',
+        80: '20rem',
+        88: '22rem',
+        96: '24rem',
+        102: '26rem'
+      }
+    }
   },
   variants: {},
-  plugins: [],
+  plugins: []
 };
