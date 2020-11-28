0ba54b8cc1fdc0ec2c8b8204d58e1487e732bf69
Milan Jovanović <milannis.995@gmail.com>
2020-11-28 21:59:55 +0100

    - Added Application.Abstractions project to extract all of the abstractions into a separate assembly

---
+++ server/Expensely.Application.Abstractions/Authentication/IJwtProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Expensely.Domain.Core;
+
+namespace Expensely.Application.Abstractions.Authentication
+{
+    /// <summary>
+    /// Represents the JWT provider interface.
+    /// </summary>
+    public interface IJwtProvider
+    {
+        /// <summary>
+        /// Creates a new JWT for the specified user.
+        /// </summary>
+        /// <param name="user">The user.</param>
+        /// <returns>The JWT for the specified user.</returns>
+        string CreateToken(User user);
+
+        /// <summary>
+        /// Creates a new refresh token.
+        /// </summary>
+        /// <returns>The refresh token and the expires on date and time in UTC format.</returns>
+        (string Token, DateTime ExpiresOnUtc) CreateRefreshToken();
+    }
+}
---
+++ server/Expensely.Application.Abstractions/Messaging/ICommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace Expensely.Application.Abstractions.Messaging
+{
+    /// <summary>
+    /// Represents the command interface.
+    /// </summary>
+    /// <typeparam name="TResponse">The command response type.</typeparam>
+    public interface ICommand<out TResponse> : IRequest<TResponse>
+    {
+    }
+}
