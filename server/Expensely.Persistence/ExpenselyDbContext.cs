fde839f2fca0ec2c02bb6d3182ff5661bbc5f251
Milan Jovanović <milannis.995@gmail.com>
2020-12-09 22:23:19 +0100

    feat(server-events): Added Application.Events.Handlers project where event handlers will live

--- server/Expensely.Persistence/ExpenselyDbContext.cs
+++ server/Expensely.Persistence/ExpenselyDbContext.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 using Expensely.Application.Abstractions.Data;
