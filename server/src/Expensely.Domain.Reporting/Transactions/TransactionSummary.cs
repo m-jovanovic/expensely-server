7a1998526f326119ba7e3310e6bb91f11c7a1d85
Milan Jovanović <milannis.995@gmail.com>
2020-12-17 17:37:05 +0100

    feat(server-reporting): Added TransactionSummary entity for reporting, added User with only Id property so that migrations can function

---
+++ server/src/Expensely.Domain.Reporting/Transactions/TransactionSummary.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Expensely.Domain.Reporting.Transactions
+{
+    /// <summary>
+    /// Represents the transaction summary entity.
+    /// </summary>
+    public sealed class TransactionSummary
+    {
+        /// <summary>
+        /// Gets the identifier.
+        /// </summary>
+        public Guid Id { get; init; }
+
+        /// <summary>
+        /// Gets the user identifier.
+        /// </summary>
+        public Guid UserId { get; init; }
+
+        /// <summary>
+        /// Gets the year.
+        /// </summary>
+        public int Year { get; init; }
+
+        /// <summary>
+        /// Gets the month.
+        /// </summary>
+        public int Month { get; init; }
+
+        /// <summary>
+        /// Gets the transaction type.
+        /// </summary>
+        public int TransactionType { get; init; }
+
+        /// <summary>
+        /// Gets the currency.
+        /// </summary>
+        public int Currency { get; init; }
+
+        /// <summary>
+        /// Gets the amount.
+        /// </summary>
+        public decimal Amount { get; init; }
+    }
+}
---
+++ server/src/Expensely.Domain.Reporting/Users/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Expensely.Domain.Reporting.Users
+{
+    /// <summary>
+    /// Represents the user entity.
+    /// </summary>
+    public sealed class User
+    {
+        /// <summary>
+        /// Gets the identifier.
+        /// </summary>
+        public Guid Id { get; init; }
+    }
+}
