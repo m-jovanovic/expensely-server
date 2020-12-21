edfba835538ccc692ca264ccf73163bf186c98b5
Milan Jovanović <milannis.995@gmail.com>
2020-12-22 00:04:19 +0100

    feat(server-transaction-summary): Implemented handling income created/changed events

---
+++ server/src/Expensely.Application.Events.Handlers/Incomes/IncomeAmountChangedEventHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Expensely.Application.Abstractions.Aggregation;
+using Expensely.Domain.Abstractions.Events;
+using Expensely.Domain.Events.Incomes;
+
+namespace Expensely.Application.Events.Handlers.Incomes
+{
+    /// <summary>
+    /// Represents the <see cref="IncomeAmountChangedEvent"/> handler.
+    /// </summary>
+    public sealed class IncomeAmountChangedEventHandler : IEventHandler<IncomeAmountChangedEvent>
+    {
+        private readonly ITransactionSummaryAggregator _transactionSummaryAggregator;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IncomeAmountChangedEventHandler"/> class.
+        /// </summary>
+        /// <param name="transactionSummaryAggregator">The transaction summary aggregator.</param>
+        public IncomeAmountChangedEventHandler(ITransactionSummaryAggregator transactionSummaryAggregator) =>
+            _transactionSummaryAggregator = transactionSummaryAggregator;
+
+        /// <inheritdoc />
+        public async Task Handle(IncomeAmountChangedEvent @event, CancellationToken cancellationToken) =>
+            await _transactionSummaryAggregator.AggregateAsync(@event.IncomeId, cancellationToken: cancellationToken);
+    }
+}
---
+++ server/src/Expensely.Application.Events.Handlers/Incomes/IncomeCreatedEventHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Expensely.Application.Abstractions.Aggregation;
+using Expensely.Domain.Abstractions.Events;
+using Expensely.Domain.Events.Incomes;
+
+namespace Expensely.Application.Events.Handlers.Incomes
+{
+    /// <summary>
+    /// Represents the <see cref="IncomeCreatedEvent"/> handler.
+    /// </summary>
+    public sealed class IncomeCreatedEventHandler : IEventHandler<IncomeCreatedEvent>
+    {
+        private readonly ITransactionSummaryAggregator _transactionSummaryAggregator;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IncomeCreatedEventHandler"/> class.
+        /// </summary>
+        /// <param name="transactionSummaryAggregator">The transaction summary aggregator.</param>
+        public IncomeCreatedEventHandler(ITransactionSummaryAggregator transactionSummaryAggregator) =>
+            _transactionSummaryAggregator = transactionSummaryAggregator;
+
+        /// <inheritdoc />
+        public async Task Handle(IncomeCreatedEvent @event, CancellationToken cancellationToken) =>
+            await _transactionSummaryAggregator.AggregateAsync(@event.IncomeId, cancellationToken: cancellationToken);
+    }
+}
