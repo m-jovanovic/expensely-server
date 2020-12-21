b34ba7f41668832508b11be4ee2d41c7cdb7178e
Milan Jovanović <milannis.995@gmail.com>
2020-12-21 23:50:37 +0100

    feat(server-transaction-summary): Implemented handling expense created/changed events

---
+++ server/src/Expensely.Application.Events.Handlers/Expenses/ExpenseAmountChangedEventHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Expensely.Application.Abstractions.Aggregation;
+using Expensely.Domain.Abstractions.Events;
+using Expensely.Domain.Events.Expenses;
+
+namespace Expensely.Application.Events.Handlers.Expenses
+{
+    /// <summary>
+    /// Represents the <see cref="ExpenseAmountChangedEvent"/> handler.
+    /// </summary>
+    public sealed class ExpenseAmountChangedEventHandler : IEventHandler<ExpenseAmountChangedEvent>
+    {
+        private readonly ITransactionSummaryAggregator _transactionSummaryAggregator;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ExpenseAmountChangedEventHandler"/> class.
+        /// </summary>
+        /// <param name="transactionSummaryAggregator">The transaction summary aggregator.</param>
+        public ExpenseAmountChangedEventHandler(ITransactionSummaryAggregator transactionSummaryAggregator) =>
+            _transactionSummaryAggregator = transactionSummaryAggregator;
+
+        /// <inheritdoc />
+        public async Task Handle(ExpenseAmountChangedEvent @event, CancellationToken cancellationToken) =>
+            await _transactionSummaryAggregator.AggregateAsync(@event.ExpenseId, cancellationToken: cancellationToken);
+    }
+}
