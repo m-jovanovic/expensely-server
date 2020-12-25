aae44b348925a10b7d861e731f565219b6edd5b3
Milan Jovanović <milannis.995@gmail.com>
2020-12-25 19:14:46 +0100

    feat(server-incomes): Implemented publishing IncomeDeletedEvent when an income is deleted

---
+++ server/src/Expensely.Domain/Events/Incomes/IncomeDeletedEvent.cs
@@ -0,0 +1,20 @@
+﻿using Expensely.Domain.Abstractions.Events;
+
+namespace Expensely.Domain.Events.Incomes
+{
+    /// <summary>
+    /// Represents the event that is raised when an income is deleted.
+    /// </summary>
+    public sealed class IncomeDeletedEvent : IEvent
+    {
+        /// <summary>
+        /// Gets the amount.
+        /// </summary>
+        public decimal Amount { get; init; }
+
+        /// <summary>
+        /// Gets the currency.
+        /// </summary>
+        public int Currency { get; init; }
+    }
+}
