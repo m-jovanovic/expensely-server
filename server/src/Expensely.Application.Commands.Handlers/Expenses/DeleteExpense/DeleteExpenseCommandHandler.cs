dd921a7ec69b36cc31588f07a77257b98bc252a6
Milan Jovanović <milannis.995@gmail.com>
2020-12-25 19:13:00 +0100

    feat(server-expenses): Implemented publishing ExpenseDeletedEvent when an expense is deleted

--- server/src/Expensely.Application.Commands.Handlers/Expenses/DeleteExpense/DeleteExpenseCommandHandler.cs
+++ server/src/Expensely.Application.Commands.Handlers/Expenses/DeleteExpense/DeleteExpenseCommandHandler.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using Expensely.Application.Abstractions.Authentication;
 using Expensely.Application.Abstractions.Data;
+using Expensely.Application.Abstractions.Messaging;
 using Expensely.Application.Commands.Expenses.DeleteExpense;
 using Expensely.Application.Commands.Handlers.Validation;
 using Expensely.Common.Abstractions.Messaging;
@@ -9,6 +10,7 @@ using Expensely.Domain.Abstractions.Maybe;
 using Expensely.Domain.Abstractions.Result;
 using Expensely.Domain.Core;
 using Expensely.Domain.Errors;
+using Expensely.Domain.Events.Expenses;
 
 namespace Expensely.Application.Commands.Handlers.Expenses.DeleteExpense
 {
@@ -19,16 +21,22 @@ namespace Expensely.Application.Commands.Handlers.Expenses.DeleteExpense
     {
         private readonly IApplicationDbContext _dbContext;
         private readonly IUserInformationProvider _userInformationProvider;
+        private readonly IEventPublisher _eventPublisher;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="DeleteExpenseCommandHandler"/> class.
         /// </summary>
         /// <param name="dbContext">The database context.</param>
         /// <param name="userInformationProvider">The user information provider.</param>
-        public DeleteExpenseCommandHandler(IApplicationDbContext dbContext, IUserInformationProvider userInformationProvider)
+        /// <param name="eventPublisher">The event publisher.</param>
+        public DeleteExpenseCommandHandler(
+            IApplicationDbContext dbContext,
+            IUserInformationProvider userInformationProvider,
+            IEventPublisher eventPublisher)
         {
             _dbContext = dbContext;
             _userInformationProvider = userInformationProvider;
+            _eventPublisher = eventPublisher;
         }
 
         /// <inheritdoc />
@@ -51,6 +59,12 @@ namespace Expensely.Application.Commands.Handlers.Expenses.DeleteExpense
             _dbContext.Remove(expense);
 
             await _dbContext.SaveChangesAsync(cancellationToken);
+
+            await _eventPublisher.PublishAsync(new ExpenseDeletedEvent
+            {
+                Amount = expense.Money.Amount,
+                Currency = expense.Money.Currency.Value
+            });
 
             return Result.Success();
         }
---
+++ server/src/Expensely.Domain/Events/Expenses/ExpenseDeletedEvent.cs
@@ -0,0 +1,20 @@
+﻿using Expensely.Domain.Abstractions.Events;
+
+namespace Expensely.Domain.Events.Expenses
+{
+    /// <summary>
+    /// Represents the event that is raised when an expense is deleted.
+    /// </summary>
+    public sealed class ExpenseDeletedEvent : IEvent
+    {
+        /// <summary>
+        /// Gets the amount.
+        /// </summary>
+        public decimal Amount { get; init; }
+
+        /// <summary>
+        /// Gets the currency.
+        /// </summary>
+        public int Currency { get; init; }
+    }
+}
