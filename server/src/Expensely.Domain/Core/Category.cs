68d805208ee587301d627e30dd2a766aeef0932c
Milan Jovanović <milannis.995@gmail.com>
2020-12-28 19:55:55 +0100

    feat(server-categories): Introduced Category enumeration that will contain the categories used for transactions

---
+++ server/src/Expensely.Domain/Core/Category.cs
@@ -0,0 +1,37 @@
+﻿using Expensely.Domain.Abstractions.Primitives;
+
+namespace Expensely.Domain.Core
+{
+    /// <summary>
+    /// Represents the category enumeration.
+    /// </summary>
+    public sealed class Category : Enumeration<Category>
+    {
+        /// <summary>
+        /// The empty category instance.
+        /// </summary>
+        internal static readonly Category None = new(default, string.Empty);
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Category"/> class.
+        /// </summary>
+        /// <param name="value">The currency value.</param>
+        /// <param name="name">The currency name.</param>
+        private Category(int value, string name)
+            : base(value, name)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Category"/> class.
+        /// </summary>
+        /// <param name="value">The category value.</param>
+        /// <remarks>
+        /// Required by EF Core.
+        /// </remarks>
+        private Category(int value)
+            : base(value, ContainsValue(value) ? FromValue(value).Value.Name : None.Name)
+        {
+        }
+    }
+}
