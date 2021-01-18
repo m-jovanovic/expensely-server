b5253ddbe5473ea69488fdace477b715a8206595
Milan Jovanović <milannis.995@gmail.com>
2021-01-18 21:01:24 +0100

    feat(server-users): Added IUserRepository interface, rewrote all user facing command handlers to use the repository

--- server/src/Expensely.Application.Commands.Handlers/Users/RemoveUserCurrency/RemoveUserCurrencyCommandHandler.cs
+++ server/src/Expensely.Application.Commands.Handlers/Users/RemoveUserCurrency/RemoveUserCurrencyCommandHandler.cs
@@ -1,12 +1,12 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
-using Expensely.Application.Abstractions.Data;
 using Expensely.Application.Commands.Users.RemoveUserCurrency;
 using Expensely.Common.Abstractions.Messaging;
 using Expensely.Domain.Abstractions.Maybe;
 using Expensely.Domain.Abstractions.Result;
 using Expensely.Domain.Core;
 using Expensely.Domain.Errors;
+using Expensely.Domain.Repositories;
 
 namespace Expensely.Application.Commands.Handlers.Users.RemoveUserCurrency
 {
@@ -15,18 +15,24 @@ namespace Expensely.Application.Commands.Handlers.Users.RemoveUserCurrency
     /// </summary>
     internal sealed class RemoveUserCurrencyCommandHandler : ICommandHandler<RemoveUserCurrencyCommand, Result>
     {
-        private readonly IApplicationDbContext _dbContext;
+        private readonly IUserRepository _userRepository;
+        private readonly IUnitOfWork _unitOfWork;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="RemoveUserCurrencyCommandHandler"/> class.
         /// </summary>
-        /// <param name="dbContext">The database context.</param>
-        public RemoveUserCurrencyCommandHandler(IApplicationDbContext dbContext) => _dbContext = dbContext;
+        /// <param name="userRepository">The user repository.</param>
+        /// <param name="unitOfWork">The unit of work.</param>
+        public RemoveUserCurrencyCommandHandler(IUserRepository userRepository, IUnitOfWork unitOfWork)
+        {
+            _userRepository = userRepository;
+            _unitOfWork = unitOfWork;
+        }
 
         /// <inheritdoc />
         public async Task<Result> Handle(RemoveUserCurrencyCommand request, CancellationToken cancellationToken)
         {
-            Maybe<User> maybeUser = await _dbContext.GetBydIdAsync<User>(request.UserId, cancellationToken);
+            Maybe<User> maybeUser = await _userRepository.GetByIdAsync(request.UserId, cancellationToken);
 
             if (maybeUser.HasNoValue)
             {
@@ -42,7 +48,7 @@ namespace Expensely.Application.Commands.Handlers.Users.RemoveUserCurrency
                 return Result.Failure(result.Error);
             }
 
-            await _dbContext.SaveChangesAsync(cancellationToken);
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
 
             return Result.Success();
         }
---
+++ server/src/Expensely.Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Expensely.Domain.Abstractions.Maybe;
+using Expensely.Domain.Core;
+
+namespace Expensely.Domain.Repositories
+{
+    /// <summary>
+    /// Represents the user repository interface.
+    /// </summary>
+    public interface IUserRepository
+    {
+        /// <summary>
+        /// Gets the user with the specified identifier, if one exists.
+        /// </summary>
+        /// <param name="userId">The user identifier.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns>The maybe instance that may contain the user with the specified identifier.</returns>
+        Task<Maybe<User>> GetByIdAsync(Guid userId, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Gets the user with the specified email, if one exists.
+        /// </summary>
+        /// <param name="email">The user email.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns>The maybe instance that may contain the user with the specified email.</returns>
+        Task<Maybe<User>> GetByEmailAsync(Email email, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Checks if any user already exists with the specified email.
+        /// </summary>
+        /// <param name="email">The email.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns>True if a user exists with the specified email, otherwise false.</returns>
+        Task<bool> AnyWithEmailAsync(Email email, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Adds the specified user to the repository.
+        /// </summary>
+        /// <param name="user">The user to be added.</param>
+        /// <returns>The completed task.</returns>
+        Task AddAsync(User user);
+    }
+}
