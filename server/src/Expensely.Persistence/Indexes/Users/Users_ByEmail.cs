f3253162b1e3533a4928afa6f442065c8284ae4f
Milan Jovanović <milannis.995@gmail.com>
2021-01-22 18:45:05 +0100

    feat(server-users): Added Users_ByEmail index and used it in the repository

---
+++ server/src/Expensely.Persistence/Indexes/Users/Users_ByEmail.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using Expensely.Domain.Core;
+using Raven.Client.Documents.Indexes;
+
+namespace Expensely.Persistence.Indexes.Users
+{
+    /// <summary>
+    /// Represents the index on users collection by email field.
+    /// </summary>
+    internal sealed class Users_ByEmail : AbstractIndexCreationTask<User>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Users_ByEmail"/> class.
+        /// </summary>
+        public Users_ByEmail() =>
+            Map = users =>
+                from user in users
+                select new
+                {
+                    Email = user.Email.Value
+                };
+    }
+}
