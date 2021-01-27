71f97a01c7cdb11aacc460d479268c7aef0adce4
Milan Jovanović <milannis.995@gmail.com>
2021-01-27 12:29:37 +0100

    feat(server-transactions): Fixed TransactionType enumeration errors

--- server/src/Expensely.Domain/Core/TransactionType.cs
+++ server/src/Expensely.Domain/Core/TransactionType.cs
@@ -7,7 +7,7 @@ namespace Expensely.Domain.Core
     /// <summary>
     /// Represents the transaction type enumeration.
     /// </summary>
-    public abstract partial class TransactionType : Enumeration<TransactionType>
+    public abstract class TransactionType : Enumeration<TransactionType>
     {
         /// <summary>
         /// The expense transaction type.
@@ -18,6 +18,16 @@ namespace Expensely.Domain.Core
         /// The income transaction type.
         /// </summary>
         public static readonly TransactionType Income = new IncomeTransactionType();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TransactionType"/> class.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <param name="name">The name.</param>
+        protected TransactionType(int value, string name)
+            : base(value, name)
+        {
+        }
 
         /// <summary>
         /// Validates that the specified monetary amount is valid for the current transaction type.
@@ -31,6 +41,14 @@ namespace Expensely.Domain.Core
         /// </summary>
         private sealed class ExpenseTransactionType : TransactionType
         {
+            /// <summary>
+            /// Initializes a new instance of the <see cref="TransactionType.ExpenseTransactionType"/> class.
+            /// </summary>
+            public ExpenseTransactionType()
+                : base(1, "Expense")
+            {
+            }
+
             /// <inheritdoc />
             public override Result ValidateAmount(Money money) =>
                 money.Amount < decimal.Zero ?
@@ -43,6 +61,14 @@ namespace Expensely.Domain.Core
         /// </summary>
         private sealed class IncomeTransactionType : TransactionType
         {
+            /// <summary>
+            /// Initializes a new instance of the <see cref="TransactionType.IncomeTransactionType"/> class.
+            /// </summary>
+            public IncomeTransactionType()
+                : base(2, "Income")
+            {
+            }
+
             /// <inheritdoc />
             public override Result ValidateAmount(Money money) =>
                 money.Amount > decimal.Zero ?
