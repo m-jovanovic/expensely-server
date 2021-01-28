0cf4e94844cae17edaab60d672fa1db5ab44e0f9
Milan Jovanović <milannis.995@gmail.com>
2021-01-28 14:08:25 +0100

    feat(server-logging): Added logging user identifier for authenticated requests

--- server/src/Expensely.Api/Behaviors/LoggingBehavior.cs
+++ server/src/Expensely.Api/Behaviors/LoggingBehavior.cs
@@ -1,6 +1,9 @@
-﻿using System.Globalization;
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Expensely.Application.Abstractions.Authentication;
 using Expensely.Application.Queries.Utility;
 using Expensely.Common.Abstractions.Clock;
 using Expensely.Common.Abstractions.Messaging;
@@ -20,17 +23,23 @@ namespace Expensely.Api.Behaviors
         where TResponse : Result
     {
         private readonly ILogger<LoggingBehavior<TRequest, TResponse>> _logger;
+        private readonly IUserInformationProvider _userInformationProvider;
         private readonly IDateTime _dateTime;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="LoggingBehavior{TRequest,TResponse}"/> class.
         /// </summary>
         /// <param name="logger">The logger.</param>
+        /// <param name="userInformationProvider">The user information provider.</param>
         /// <param name="dateTime">The date and time.</param>
-        public LoggingBehavior(ILogger<LoggingBehavior<TRequest, TResponse>> logger, IDateTime dateTime)
+        public LoggingBehavior(
+            ILogger<LoggingBehavior<TRequest, TResponse>> logger,
+            IUserInformationProvider userInformationProvider,
+            IDateTime dateTime)
         {
             _logger = logger;
             _dateTime = dateTime;
+            _userInformationProvider = userInformationProvider;
         }
 
         /// <inheritdoc />
@@ -38,19 +47,35 @@ namespace Expensely.Api.Behaviors
         {
             string requestName = request.GetType().Name;
 
-            _logger.LogInformation(
-                "Request started {@Request} {@UtcNow}.",
-                requestName,
-                _dateTime.UtcNow.ToString(DateTimeFormats.DateTimeWithMilliseconds, CultureInfo.InvariantCulture));
+            _logger.LogInformation(GetRequestStartedLogMessage(), GetLogArguments(requestName).ToArray());
 
             TResponse response = await next();
 
-            _logger.LogInformation(
-                "Request completed {@Request} {@UtcNow}.",
-                requestName,
-                _dateTime.UtcNow.ToString(DateTimeFormats.DateTimeWithMilliseconds, CultureInfo.InvariantCulture));
+            _logger.LogInformation(GetRequestCompletedLogMessage(), GetLogArguments(requestName).ToArray());
 
             return response;
+        }
+
+        private string GetRequestStartedLogMessage() =>
+            _userInformationProvider.IsAuthenticated
+                ? "Request started {@UserId} {@Request} {@UtcNow}."
+                : "Request started {@Request} {@UtcNow}.";
+
+        private string GetRequestCompletedLogMessage() =>
+            _userInformationProvider.IsAuthenticated
+                ? "Request completed {@UserId} {@Request} {@UtcNow}."
+                : "Request completed {@Request} {@UtcNow}.";
+
+        private IEnumerable<object> GetLogArguments(string requestName)
+        {
+            if (_userInformationProvider.IsAuthenticated)
+            {
+                yield return _userInformationProvider.UserId;
+            }
+
+            yield return requestName;
+
+            yield return _dateTime.UtcNow.ToString(DateTimeFormats.DateTimeWithMilliseconds, CultureInfo.InvariantCulture);
         }
     }
 }
