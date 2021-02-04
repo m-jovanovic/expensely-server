38d25df1be41eb7db9d29879682242a15d00feea
Milan Jovanović <milannis.995@gmail.com>
2021-02-04 17:35:03 +0100

    feat(client-packages): Successfully upgraded tailwind to v2

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,17 +1,11 @@
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        loader: 'postcss-loader',
-        options: {
-          postcssOptions: {
-            ident: 'postcss',
-            syntax: 'postcss-scss',
-            plugins: [require('postcss-import'), require('tailwindcss'), require('autoprefixer')]
-          }
-        }
-      }
-    ]
-  }
+const { addTailwindPlugin } = require("@ngneat/tailwind");
+const tailwindConfig = require("./tailwind.config.js");
+
+module.exports = (config) => {
+  addTailwindPlugin({
+    webpackConfig: config,
+    tailwindConfig,
+    patchComponentsStyles: true
+  });
+  return config;
 };
