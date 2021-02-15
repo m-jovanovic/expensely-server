b05c3b8b08ec419e6abba8d98861e21fb69f3ea1
Milan Jovanović <milannis.995@gmail.com>
2021-02-15 20:32:14 +0100

    feat(server-services): Added AuthorizationServiceInstaller

---
+++ server/src/Expensely.Api/ServiceInstallers/Authorization/AuthorizationServiceInstaller.cs
@@ -0,0 +1,14 @@
+﻿using Expensely.Api.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Expensely.Api.ServiceInstallers.Authorization
+{
+    /// <summary>
+    /// Represents the authorization services installer.
+    /// </summary>
+    public sealed class AuthorizationServiceInstaller : IServiceInstaller
+    {
+        /// <inheritdoc />
+        public void InstallServices(IServiceCollection services) => services.AddAuthorization();
+    }
+}
