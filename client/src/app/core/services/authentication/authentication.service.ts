b72349365c0bd355dcb382df0000c5e1e1085542
Milan Jovanović <milannis.995@gmail.com>
2021-02-21 21:21:23 +0100

    feat(client-authentication): Added timer in AuthenticationService that executes the refresh token action 1 minute before the access token is supposed to expire

--- client/src/app/core/services/authentication/authentication.service.ts
+++ client/src/app/core/services/authentication/authentication.service.ts
@@ -5,29 +5,34 @@ import { first, tap } from 'rxjs/operators';
 
 import { ApiRoutes } from '../../constants/api-routes';
 import { ApiService } from '../api/api.service';
-import { LoginRequest, RefreshTokenRequest, RegisterRequest, TokenResponse } from '../../contracts';
 import { RouterService } from '../common/router.service';
+import { JwtService } from '../common/jwt-service';
+import { LoginRequest, RefreshTokenRequest, RegisterRequest, TokenResponse } from '../../contracts';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService extends ApiService {
-  constructor(client: HttpClient, private routerService: RouterService) {
+  private refreshTokenTimer;
+
+  constructor(client: HttpClient, private routerService: RouterService, private jwtService: JwtService) {
     super(client);
   }
 
   login(request: LoginRequest): Observable<TokenResponse> {
     return this.post<TokenResponse>(ApiRoutes.Authentication.login, request).pipe(
       first(),
-      tap((response: TokenResponse) => {
-        if (response.token) {
-          this.routerService.navigate(['']);
-        }
+      tap((tokenResponse: TokenResponse) => {
+        this.startRefreshTokenTimer(tokenResponse);
+
+        this.routerService.navigate(['']);
       })
     );
   }
 
   logout(): Observable<any> {
+    this.stopRefreshTokenTimer();
+
     return this.routerService.navigate(['/login']);
   }
 
@@ -36,6 +41,32 @@ export class AuthenticationService extends ApiService {
   }
 
   refreshToken(request: RefreshTokenRequest): Observable<TokenResponse> {
-    return this.post<TokenResponse>(ApiRoutes.Authentication.refreshToken, request).pipe(first());
+    return this.post<TokenResponse>(ApiRoutes.Authentication.refreshToken, request).pipe(
+      first(),
+      tap((tokenResponse: TokenResponse) => {
+        this.startRefreshTokenTimer(tokenResponse);
+      })
+    );
+  }
+
+  private startRefreshTokenTimer(tokenResponse: TokenResponse): void {
+    const tokenInfo = this.jwtService.decodeToken(tokenResponse.token);
+
+    const tokenExpiresOn = new Date(tokenInfo.exp);
+
+    const oneMinuteInMilliseconds = 60 * 1000;
+
+    const tokenExpirationInMilliseconds = tokenExpiresOn.getTime() - Date.now();
+
+    const timeoutInMilliseconds = tokenExpirationInMilliseconds - oneMinuteInMilliseconds;
+
+    this.refreshTokenTimer = setTimeout(
+      () => this.refreshToken(new RefreshTokenRequest(tokenResponse.refreshToken)).subscribe(),
+      timeoutInMilliseconds
+    );
+  }
+
+  private stopRefreshTokenTimer(): void {
+    clearTimeout(this.refreshTokenTimer);
   }
 }
