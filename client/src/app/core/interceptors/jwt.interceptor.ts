a3434100f5ad83d13e01d72c8598496de7111a58
Milan Jovanović <milannis.995@gmail.com>
2021-02-21 21:18:22 +0100

    feat(client-interceptors): Improved JwtInterceptor to only set Authorization header for requests going the backend API

--- client/src/app/core/interceptors/jwt.interceptor.ts
+++ client/src/app/core/interceptors/jwt.interceptor.ts
@@ -3,6 +3,7 @@ import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/c
 import { Observable } from 'rxjs';
 
 import { AuthenticationFacade } from '../store';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -11,11 +12,13 @@ export class JwtInterceptor implements HttpInterceptor {
   constructor(private authenticationFacade: AuthenticationFacade) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    req = req.clone({
-      setHeaders: {
-        Authorization: `Bearer ${this.authenticationFacade.token}`
-      }
-    });
+    if (req.url.startsWith(environment.apiUrl)) {
+      req = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${this.authenticationFacade.token}`
+        }
+      });
+    }
 
     return next.handle(req);
   }
