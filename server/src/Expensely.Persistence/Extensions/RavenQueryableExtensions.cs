b1672bf336ee85b7aad171b70598691708996324
Milan Jovanović <milannis.995@gmail.com>
2021-02-24 20:50:46 +0100

    feat(server-transactions): Implemented GetTransactionByIdQueryProcessor

---
+++ server/src/Expensely.Persistence/Extensions/RavenQueryableExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Raven.Client.Documents.Linq;
+
+namespace Expensely.Persistence.Extensions
+{
+    /// <summary>
+    /// Contains the extension methods for the <see cref="IRavenQueryable{T}"/> class.
+    /// </summary>
+    internal static class RavenQueryableExtensions
+    {
+        /// <summary>
+        /// Customizes the <see cref="IRavenQueryable{T}"/> to not track entities.
+        /// </summary>
+        /// <typeparam name="T">The query result type.</typeparam>
+        /// <param name="query">The query instance.</param>
+        /// <returns>The customized query.</returns>
+        internal static IRavenQueryable<T> WithNoTracking<T>(this IRavenQueryable<T> query) => query.Customize(x => x.NoTracking());
+    }
+}
---
+++ server/src/Expensely.Persistence/QueryProcessors/Transactions/GetTransactionByIdQueryProcessor.cs
@@ -0,0 +1,54 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Expensely.Application.Abstractions.Authentication;
+using Expensely.Application.Contracts.Transactions;
+using Expensely.Application.Queries.Processors.Transactions;
+using Expensely.Application.Queries.Transactions;
+using Expensely.Common.Primitives.Maybe;
+using Expensely.Domain.Modules.Transactions;
+using Raven.Client.Documents.Session;
+
+namespace Expensely.Persistence.QueryProcessors.Transactions
+{
+    /// <summary>
+    /// Represents the <see cref="GetTransactionByIdQuery"/> processor.
+    /// </summary>
+    internal sealed class GetTransactionByIdQueryProcessor : IGetTransactionByIdQueryProcessor
+    {
+        private readonly IAsyncDocumentSession _session;
+        private readonly IUserInformationProvider _userInformationProvider;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GetTransactionByIdQueryProcessor"/> class.
+        /// </summary>
+        /// <param name="session">The document session.</param>
+        /// <param name="userInformationProvider">The user information provider.</param>
+        public GetTransactionByIdQueryProcessor(IAsyncDocumentSession session, IUserInformationProvider userInformationProvider)
+        {
+            _session = session;
+            _userInformationProvider = userInformationProvider;
+        }
+
+        /// <inheritdoc />
+        public async Task<Maybe<TransactionResponse>> Process(GetTransactionByIdQuery query, CancellationToken cancellationToken = default)
+        {
+            Maybe<Transaction> maybeTransaction = await _session.LoadAsync<Transaction>(query.TransactionId, cancellationToken);
+
+            if (maybeTransaction.HasNoValue || maybeTransaction.Value.UserId != _userInformationProvider.UserId)
+            {
+                return Maybe<TransactionResponse>.None;
+            }
+
+            Transaction transaction = maybeTransaction.Value;
+
+            var transactionResponse = new TransactionResponse(
+                transaction.Id,
+                transaction.Description,
+                transaction.Category,
+                transaction.Money,
+                transaction.OccurredOn);
+
+            return transactionResponse;
+        }
+    }
+}
