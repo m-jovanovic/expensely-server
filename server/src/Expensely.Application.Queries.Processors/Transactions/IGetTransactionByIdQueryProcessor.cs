78ec85b3ad9d28130d7b3c7e4e46b882eca277ff
Milan Jovanović <milannis.995@gmail.com>
2021-02-24 20:48:47 +0100

    feat(server-transactions): Added GetTransactionByIdQuery and query handler, added interface for query processor

---
+++ server/src/Expensely.Application.Queries.Processors/Transactions/IGetTransactionByIdQueryProcessor.cs
@@ -0,0 +1,14 @@
+﻿using Expensely.Application.Contracts.Transactions;
+using Expensely.Application.Queries.Processors.Abstractions;
+using Expensely.Application.Queries.Transactions;
+using Expensely.Common.Primitives.Maybe;
+
+namespace Expensely.Application.Queries.Processors.Transactions
+{
+    /// <summary>
+    /// Represents the <see cref="GetTransactionByIdQuery"/> processor interface.
+    /// </summary>
+    public interface IGetTransactionByIdQueryProcessor : IQueryProcessor<GetTransactionByIdQuery, Maybe<TransactionResponse>>
+    {
+    }
+}
---
+++ server/src/Expensely.Application.Queries/Transactions/GetTransactionByIdQuery.cs
@@ -0,0 +1,11 @@
+﻿using Expensely.Application.Contracts.Transactions;
+using Expensely.Common.Abstractions.Messaging;
+using Expensely.Common.Primitives.Maybe;
+
+namespace Expensely.Application.Queries.Transactions
+{
+    /// <summary>
+    /// Represents the query for getting a transaction by identifier.
+    /// </summary>
+    public sealed record GetTransactionByIdQuery(string TransactionId) : IQuery<Maybe<TransactionResponse>>;
+}
