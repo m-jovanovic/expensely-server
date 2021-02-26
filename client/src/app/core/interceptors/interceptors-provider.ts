faad1618253b31ecdb45f3805b9e065dcdc20948
Milan Jovanović <milannis.995@gmail.com>
2021-02-26 10:22:32 +0100

    feat(client-authentication): Integrated RefreshTokenIntereceptor into JwtInterceptor, and added blocking HTTP requests while waiting for JWT token to be refreshed

--- client/src/app/core/interceptors/interceptors-provider.ts
+++ client/src/app/core/interceptors/interceptors-provider.ts
@@ -1,16 +1,10 @@
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { JwtInterceptor } from './jwt.interceptor';
-import { RefreshTokenInterceptor } from './refresh-token-interceptor';
 
 export const interceptorsProvider = [
   {
     provide: HTTP_INTERCEPTORS,
     useClass: JwtInterceptor,
-    multi: true
-  },
-  {
-    provide: HTTP_INTERCEPTORS,
-    useClass: RefreshTokenInterceptor,
     multi: true
   }
 ];
--- client/src/app/core/interceptors/jwt.interceptor.ts
+++ client/src/app/core/interceptors/jwt.interceptor.ts
@@ -1,25 +1,70 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse } from '@angular/common/http';
+import { BehaviorSubject, Observable, throwError } from 'rxjs';
 
 import { AuthenticationFacade } from '../store';
 import { environment } from '../../../environments/environment';
+import { catchError, filter, switchMap, take } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class JwtInterceptor implements HttpInterceptor {
+  private readonly unauthorizedStatusCode = 401;
+  private refreshTokenExecuting = false;
+  private refreshTokenSubject = new BehaviorSubject<Object>(null);
+
   constructor(private authenticationFacade: AuthenticationFacade) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    if (req.url.startsWith(environment.apiUrl)) {
-      req = req.clone({
-        setHeaders: {
-          Authorization: `Bearer ${this.authenticationFacade.token}`
-        }
-      });
+    if (!req.url.startsWith(environment.apiUrl)) {
+      return next.handle(req);
     }
 
-    return next.handle(req);
+    req = this.addAuthorizationHeader(req);
+
+    return next.handle(req).pipe(
+      catchError((error: HttpErrorResponse) => {
+        if (error.status !== this.unauthorizedStatusCode) {
+          return throwError(error);
+        }
+
+        return this.handle401Unauthorized(req, next);
+      })
+    );
+  }
+
+  private handle401Unauthorized(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if (this.refreshTokenExecuting) {
+      return this.refreshTokenSubject.pipe(
+        filter((value) => value != null),
+        take(1),
+        switchMap(() => {
+          return next.handle(this.addAuthorizationHeader(req));
+        })
+      );
+    }
+
+    this.refreshTokenExecuting = true;
+
+    this.refreshTokenSubject.next(null);
+
+    return this.authenticationFacade.refreshToken().pipe(
+      switchMap(() => {
+        this.refreshTokenExecuting = false;
+
+        this.refreshTokenSubject.next(new Object());
+
+        return next.handle(this.addAuthorizationHeader(req));
+      })
+    );
+  }
+
+  private addAuthorizationHeader(request: HttpRequest<any>): HttpRequest<any> {
+    return request.clone({
+      setHeaders: {
+        Authorization: `Bearer ${this.authenticationFacade.token}`
+      }
+    });
   }
 }
