43226faff5d2b05cb9ffbb8cee9730a7fb45d962
Milan Jovanović <milannis.995@gmail.com>
2021-03-06 11:14:54 +0100

    feat(server-events): Added non-generic IEventHandler interface and abstract generic EventHandler that delegates the non-generic Handle to the generic one

---
+++ server/src/Expensely.Domain/Abstractions/EventHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Expensely.Domain.Abstractions
+{
+    /// <summary>
+    /// Represents the abstract event handler.
+    /// </summary>
+    /// <typeparam name="TEvent">The event type.</typeparam>
+    public abstract class EventHandler<TEvent> : IEventHandler<TEvent>, IEventHandler
+        where TEvent : IEvent
+    {
+        /// <inheritdoc />
+        public abstract Task Handle(TEvent @event, CancellationToken cancellationToken = default);
+
+        /// <inheritdoc />
+        public Task Handle(IEvent @event, CancellationToken cancellationToken = default) => Handle((TEvent)@event, cancellationToken);
+    }
+}
--- server/src/Expensely.Domain/Abstractions/IEventHandler.cs
+++ server/src/Expensely.Domain/Abstractions/IEventHandler.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 namespace Expensely.Domain.Abstractions
 {
     /// <summary>
-    /// Represents the handler for a specific event.
+    /// Represents the event handler for a specific event.
     /// </summary>
     /// <typeparam name="TEvent">The event type.</typeparam>
     public interface IEventHandler<in TEvent>
@@ -17,5 +17,19 @@ namespace Expensely.Domain.Abstractions
         /// <param name="cancellationToken">The cancellation token.</param>
         /// <returns>The completed task.</returns>
         Task Handle(TEvent @event, CancellationToken cancellationToken = default);
+    }
+
+    /// <summary>
+    /// Represents the event handler for a specific event.
+    /// </summary>
+    public interface IEventHandler
+    {
+        /// <summary>
+        /// Handles the specified event.
+        /// </summary>
+        /// <param name="event">The event.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns>The completed task.</returns>
+        Task Handle(IEvent @event, CancellationToken cancellationToken = default);
     }
 }
