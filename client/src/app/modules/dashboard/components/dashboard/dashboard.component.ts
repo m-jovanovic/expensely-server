a98f7f0a2fdbf5cb3fd257da079292aa97548eab
Milan Jovanović <milannis.995@gmail.com>
2021-03-10 18:43:06 +0100

    feat(client-dashboard): Removed ability to delete transactions from dashboard page

--- client/src/app/modules/dashboard/components/dashboard/dashboard.component.ts
+++ client/src/app/modules/dashboard/components/dashboard/dashboard.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
 import { TransactionListFacade, TransactionSummaryFacade } from '@expensely/core';
-import { ConfirmationDialogService } from '@expensely/shared/services';
 
 @Component({
   selector: 'exp-dashboard',
@@ -11,31 +10,11 @@ import { ConfirmationDialogService } from '@expensely/shared/services';
 export class DashboardComponent implements OnInit {
   private readonly numberOfTransactions = 10;
 
-  constructor(
-    public transactionListFacade: TransactionListFacade,
-    public transactionSummaryFacade: TransactionSummaryFacade,
-    private confirmationDialogService: ConfirmationDialogService
-  ) {}
+  constructor(public transactionListFacade: TransactionListFacade, public transactionSummaryFacade: TransactionSummaryFacade) {}
 
   ngOnInit(): void {
     this.transactionListFacade.loadTransactions(this.numberOfTransactions);
 
     this.transactionSummaryFacade.loadTransactionSummary();
-  }
-
-  deleteTransaction(transactionId: string): void {
-    this.confirmationDialogService.open({
-      message: 'Are you sure you want to delete this transaction?',
-      cancelButtonText: 'Cancel',
-      confirmButtonText: 'Delete'
-    });
-
-    this.confirmationDialogService.afterClosed().subscribe((confirmed) => {
-      if (!confirmed) {
-        return;
-      }
-
-      this.transactionListFacade.deleteTransaction(transactionId);
-    });
   }
 }
