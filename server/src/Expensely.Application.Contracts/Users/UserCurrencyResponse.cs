6c38e18a4f3d86ea43e55eadd62482a8861b71f7
Milan Jovanović <milannis.995@gmail.com>
2021-03-12 13:49:27 +0100

    feat(server-users): Added GetUserCurrenciesQuery query and UserCurrencyResponse contract

---
+++ server/src/Expensely.Application.Contracts/Users/UserCurrencyResponse.cs
@@ -0,0 +1,28 @@
+﻿namespace Expensely.Application.Contracts.Users
+{
+    /// <summary>
+    /// Represents the user currency response.
+    /// </summary>
+    public sealed class UserCurrencyResponse
+    {
+        /// <summary>
+        /// Gets the currency identifier.
+        /// </summary>
+        public int Id { get; init; }
+
+        /// <summary>
+        /// Gets the currency name.
+        /// </summary>
+        public string Name { get; init; }
+
+        /// <summary>
+        /// Gets the currency code.
+        /// </summary>
+        public string Code { get; init; }
+
+        /// <summary>
+        /// Gets a value indicating whether or not the currency is the user's primary currency.
+        /// </summary>
+        public bool IsPrimary { get; init; }
+    }
+}
---
+++ server/src/Expensely.Application.Queries/Users/GetUserCurrenciesQuery.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Expensely.Application.Contracts.Users;
+using Expensely.Common.Abstractions.Messaging;
+using Expensely.Common.Primitives.Maybe;
+
+namespace Expensely.Application.Queries.Users
+{
+    /// <summary>
+    /// Represents the query for getting a user's currencies.
+    /// </summary>
+    public sealed class GetUserCurrenciesQuery : IQuery<Maybe<IReadOnlyCollection<UserCurrencyResponse>>>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GetUserCurrenciesQuery"/> class.
+        /// </summary>
+        /// <param name="userId">The user identifier.</param>
+        public GetUserCurrenciesQuery(Guid userId) => UserId = userId.ToString();
+
+        /// <summary>
+        /// Gets the user identifier.
+        /// </summary>
+        public string UserId { get; }
+    }
+}
