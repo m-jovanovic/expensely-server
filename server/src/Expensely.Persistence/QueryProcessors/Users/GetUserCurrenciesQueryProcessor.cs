d732b35f87fb2d5cd3d0801716e21fa319bdf444
Milan Jovanović <milannis.995@gmail.com>
2021-03-12 16:39:42 +0100

    feat(server-users): Implemented GetUserCurrenciesQueryProcessor

--- server/src/Expensely.Persistence/QueryProcessors/Users/GetUserCurrenciesQueryProcessor.cs
+++ server/src/Expensely.Persistence/QueryProcessors/Users/GetUserCurrenciesQueryProcessor.cs
@@ -1,11 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Expensely.Application.Abstractions.Authentication;
 using Expensely.Application.Contracts.Users;
 using Expensely.Application.Queries.Processors.Users;
 using Expensely.Application.Queries.Users;
 using Expensely.Common.Primitives.Maybe;
+using Expensely.Domain.Modules.Users;
+using Raven.Client.Documents;
+using Raven.Client.Documents.Session;
 
 namespace Expensely.Persistence.QueryProcessors.Users
 {
@@ -14,10 +18,56 @@ namespace Expensely.Persistence.QueryProcessors.Users
     /// </summary>
     public sealed class GetUserCurrenciesQueryProcessor : IGetUserCurrenciesQueryProcessor
     {
+        private readonly IAsyncDocumentSession _session;
+        private readonly IUserInformationProvider _userInformationProvider;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GetUserCurrenciesQueryProcessor"/> class.
+        /// </summary>
+        /// <param name="session">The document session.</param>
+        /// <param name="userInformationProvider">The user information provider.</param>
+        public GetUserCurrenciesQueryProcessor(IAsyncDocumentSession session, IUserInformationProvider userInformationProvider)
+        {
+            _session = session;
+            _userInformationProvider = userInformationProvider;
+        }
+
         /// <inheritdoc />
-        public Task<Maybe<IReadOnlyCollection<UserCurrencyResponse>>> Process(
+        public async Task<Maybe<IReadOnlyCollection<UserCurrencyResponse>>> Process(
             GetUserCurrenciesQuery query,
-            CancellationToken cancellationToken = default) =>
-            throw new NotImplementedException();
+            CancellationToken cancellationToken = default)
+        {
+            if (query.UserId != _userInformationProvider.UserId)
+            {
+                return Maybe<IReadOnlyCollection<UserCurrencyResponse>>.None;
+            }
+
+            var userCurrencies = await _session.Query<User>()
+                .Where(x => x.Id == query.UserId)
+                .Select(x => new
+                {
+                    x.PrimaryCurrency,
+                    x.Currencies
+                })
+                .FirstOrDefaultAsync(cancellationToken);
+
+            if (userCurrencies is null)
+            {
+                return Maybe<IReadOnlyCollection<UserCurrencyResponse>>.None;
+            }
+
+            UserCurrencyResponse[] userCurrencyResponses = userCurrencies.Currencies
+                .Select(x => new UserCurrencyResponse
+                {
+                    Id = x.Value,
+                    Code = x.Code,
+                    Name = x.Name,
+                    IsPrimary = x.Value == userCurrencies.PrimaryCurrency.Value
+                })
+                .OrderByDescending(x => x.IsPrimary)
+                .ToArray();
+
+            return userCurrencyResponses;
+        }
     }
 }
