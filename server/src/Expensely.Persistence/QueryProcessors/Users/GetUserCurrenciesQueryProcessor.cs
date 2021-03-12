9e913e937f8e0428423366d2dfa175c9097e07df
Milan Jovanović <milannis.995@gmail.com>
2021-03-12 13:49:48 +0100

    feat(server-users): Added GetUserCurrenciesQueryHandler and stubbed out IGetUserCurrenciesQueryProcessor implementation

---
+++ server/src/Expensely.Persistence/QueryProcessors/Users/GetUserCurrenciesQueryProcessor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Expensely.Application.Contracts.Users;
+using Expensely.Application.Queries.Processors.Users;
+using Expensely.Application.Queries.Users;
+using Expensely.Common.Primitives.Maybe;
+
+namespace Expensely.Persistence.QueryProcessors.Users
+{
+    /// <summary>
+    /// Represents the <see cref="GetUserCurrenciesQuery"/> processor.
+    /// </summary>
+    public sealed class GetUserCurrenciesQueryProcessor : IGetUserCurrenciesQueryProcessor
+    {
+        /// <inheritdoc />
+        public Task<Maybe<IReadOnlyCollection<UserCurrencyResponse>>> Process(
+            GetUserCurrenciesQuery query,
+            CancellationToken cancellationToken = default) =>
+            throw new NotImplementedException();
+    }
+}
