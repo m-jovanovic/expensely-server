34213a4a804a238a068d8aaad8ee5bed58a09c07
Milan Jovanović <milannis.995@gmail.com>
2021-03-15 20:07:22 +0100

    feat(server-authorization): Added Infrastructure.Authorization project to encapsulate logic related to authorization

---
+++ server/src/Expensely.Infrastructure.Authorization/Attributes/HasPermissionAttribute.cs
@@ -0,0 +1,20 @@
+﻿using Expensely.Domain.Modules.Permissions;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Expensely.Infrastructure.Authorization.Attributes
+{
+    /// <summary>
+    /// Represents the attribute for authorizing an action based on the <see cref="Permission"/>.
+    /// </summary>
+    public sealed class HasPermissionAttribute : AuthorizeAttribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HasPermissionAttribute"/> class.
+        /// </summary>
+        /// <param name="permission">The permission that is required to perform the action.</param>
+        public HasPermissionAttribute(Permission permission)
+            : base(permission.ToString())
+        {
+        }
+    }
+}
