a264a559a40609825eba68ee3cab986d784e26be
Milan Jovanović <milannis.995@gmail.com>
2021-03-15 20:23:57 +0100

    feat(server-authorization): Added PermissionRequirement and PermissionAuthorizationPolicyProvider

---
+++ server/src/Expensely.Infrastructure.Authorization/Providers/PermissionAuthorizationPolicyProvider.cs
@@ -0,0 +1,43 @@
+﻿using System.Threading.Tasks;
+using Expensely.Infrastructure.Authorization.Requirements;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Options;
+
+namespace Expensely.Infrastructure.Authorization.Providers
+{
+    /// <summary>
+    /// Represents the permission authorization policy provider.
+    /// </summary>
+    public sealed class PermissionAuthorizationPolicyProvider : DefaultAuthorizationPolicyProvider
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PermissionAuthorizationPolicyProvider"/> class.
+        /// </summary>
+        /// <param name="options">The authorization options.</param>
+        public PermissionAuthorizationPolicyProvider(IOptions<AuthorizationOptions> options)
+            : base(options)
+        {
+        }
+
+        /// <inheritdoc />
+        public override async Task<AuthorizationPolicy> GetPolicyAsync(string policyName)
+        {
+            AuthorizationPolicy authorizationPolicy = await base.GetPolicyAsync(policyName);
+
+            if (authorizationPolicy is not null)
+            {
+                return authorizationPolicy;
+            }
+
+            var permissionRequirement = new PermissionRequirement(policyName);
+
+            var authorizationPolicyBuilder = new AuthorizationPolicyBuilder();
+
+            authorizationPolicyBuilder.AddRequirements(permissionRequirement);
+
+            AuthorizationPolicy newAuthorizationPolicy = authorizationPolicyBuilder.Build();
+
+            return newAuthorizationPolicy;
+        }
+    }
+}
---
+++ server/src/Expensely.Infrastructure.Authorization/Requirements/PermissionRequirement.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace Expensely.Infrastructure.Authorization.Requirements
+{
+    /// <summary>
+    /// Represents the permission authorization requirement.
+    /// </summary>
+    internal sealed class PermissionRequirement : IAuthorizationRequirement
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PermissionRequirement"/> class.
+        /// </summary>
+        /// <param name="permissionName">The permission name.</param>
+        internal PermissionRequirement(string permissionName) => PermissionName = permissionName;
+
+        /// <summary>
+        /// Gets the permission name.
+        /// </summary>
+        internal string PermissionName { get; }
+    }
+}
