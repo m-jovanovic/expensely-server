cbcdeaa52439759e25a1392412aab023e82aae74
Milan Jovanović <milannis.995@gmail.com>
2021-03-16 17:23:11 +0100

    feat(client-authorization): Registered the necessary services with the DI container

---
+++ server/src/Expensely.Authorization/AuthorizationAssembly.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+
+namespace Expensely.Authorization
+{
+    /// <summary>
+    /// Represents the authorization assembly.
+    /// </summary>
+    public static class AuthorizationAssembly
+    {
+        /// <summary>
+        /// Gets the authorization assembly.
+        /// </summary>
+        public static readonly Assembly Assembly = Assembly.GetExecutingAssembly();
+    }
+}
--- server/src/Expensely.WebApp/ServiceInstallers/Authorization/AuthorizationServiceInstaller.cs
+++ server/src/Expensely.WebApp/ServiceInstallers/Authorization/AuthorizationServiceInstaller.cs
@@ -1,4 +1,6 @@
-﻿using Expensely.WebApp.Abstractions;
+﻿using Expensely.Authorization;
+using Expensely.WebApp.Abstractions;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Expensely.WebApp.ServiceInstallers.Authorization
@@ -9,6 +11,26 @@ namespace Expensely.WebApp.ServiceInstallers.Authorization
     public sealed class AuthorizationServiceInstaller : IServiceInstaller
     {
         /// <inheritdoc />
-        public void InstallServices(IServiceCollection services) => services.AddAuthorization();
+        public void InstallServices(IServiceCollection services)
+        {
+            services.AddAuthorization();
+
+            AddAuthorizationPolicyProvider(services);
+
+            AddAuthorizationHandler(services);
+        }
+
+        private static void AddAuthorizationPolicyProvider(IServiceCollection services) =>
+            services.Scan(x =>
+                x.FromAssemblies(AuthorizationAssembly.Assembly)
+                    .AddClasses(x => x.AssignableTo<IAuthorizationPolicyProvider>(), false)
+                    .AsImplementedInterfaces()
+                    .WithSingletonLifetime());
+
+        private static void AddAuthorizationHandler(IServiceCollection services) =>
+            services.Scan(x => x.FromAssemblies(AuthorizationAssembly.Assembly)
+                .AddClasses(x => x.AssignableTo<IAuthorizationHandler>(), false)
+                .AsImplementedInterfaces()
+                .WithSingletonLifetime());
     }
 }
