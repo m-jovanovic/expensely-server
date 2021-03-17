9e695263f2609f28e6b26408d912d6ae2f361af6
Milan Jovanović <milannis.995@gmail.com>
2021-03-17 15:51:27 +0100

    feat(server-domain): Removed documentation from private members in TransactionType

--- server/src/Expensely.Domain/Modules/Transactions/TransactionType.cs
+++ server/src/Expensely.Domain/Modules/Transactions/TransactionType.cs
@@ -37,40 +37,26 @@ namespace Expensely.Domain.Modules.Transactions
         /// <returns>The success result if the validation was successful, otherwise an error result.</returns>
         public abstract Result ValidateAmount(Money money);
 
-        /// <summary>
-        /// Represents the expense transaction type.
-        /// </summary>
         private sealed class ExpenseTransactionType : TransactionType
         {
-            /// <summary>
-            /// Initializes a new instance of the <see cref="TransactionType.ExpenseTransactionType"/> class.
-            /// </summary>
             public ExpenseTransactionType()
-                : base(1, "Expense")
+                : base(1, nameof(Expense))
             {
             }
 
-            /// <inheritdoc />
             public override Result ValidateAmount(Money money) =>
                 money.Amount < decimal.Zero ?
                     Result.Success() :
                     Result.Failure(DomainErrors.Transaction.ExpenseAmountGreaterThanOrEqualToZero);
         }
 
-        /// <summary>
-        /// Represents the income transaction type.
-        /// </summary>
         private sealed class IncomeTransactionType : TransactionType
         {
-            /// <summary>
-            /// Initializes a new instance of the <see cref="TransactionType.IncomeTransactionType"/> class.
-            /// </summary>
             public IncomeTransactionType()
-                : base(2, "Income")
+                : base(2, nameof(Income))
             {
             }
 
-            /// <inheritdoc />
             public override Result ValidateAmount(Money money) =>
                 money.Amount > decimal.Zero ?
                     Result.Success() :
