0548581bf0dabcf0af203bc11f7e1fcc3c4d5324
Milan Jovanović <milannis.995@gmail.com>
2021-03-27 18:49:52 +0100

    feat(server-test): Added Stylecop to test projects and cleaned up GlobalSuppressions rules

--- server/test/Expensely.Application.UnitTests/GlobalSuppressions.cs
+++ server/test/Expensely.Application.UnitTests/GlobalSuppressions.cs
@@ -7,15 +7,3 @@
         Justification = "Unit test project.",
         Scope = "namespaceanddescendants",
         Target = "~N:Expensely.Application.UnitTests")]
-
-[assembly:
-    SuppressMessage(
-        "StyleCop.CSharp.OrderingRules",
-        "SA1200:Using directives should be placed correctly",
-        Justification = "Unit test project.")]
-
-[assembly:
-    SuppressMessage(
-        "StyleCop.CSharp.DocumentationRules",
-        "SA1633:File should have header",
-        Justification = "Unit test project.")]
