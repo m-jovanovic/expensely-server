a8c2fec110d2fe067fca6f93348031227b18bab0
Milan Jovanović <milannis.995@gmail.com>
2021-04-01 18:21:18 +0200

    feat(client-budgets): Added initial form mock-up for creating budgets

--- client/src/app/modules/budgets/pages/create-budget/create-budget.component.ts
+++ client/src/app/modules/budgets/pages/create-budget/create-budget.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
+
+import { BudgetFacade, CategoryFacade, CategoryResponse, CurrencyResponse, UserFacade } from '@expensely/core';
 
 @Component({
   selector: 'exp-create-budget',
@@ -6,7 +10,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-budget.component.scss']
 })
 export class CreateBudgetComponent implements OnInit {
-  constructor() {}
+  createBudgetForm: FormGroup;
+  currencies$: Observable<CurrencyResponse[]>;
+  categories$: Observable<CategoryResponse[]>;
+  isLoading$: Observable<boolean>;
+  submitted = false;
 
-  ngOnInit(): void {}
+  constructor(
+    private budgetFacade: BudgetFacade,
+    private userFacade: UserFacade,
+    private categoryFacade: CategoryFacade,
+    private formBuilder: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    this.createBudgetForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.maxLength(100)]],
+      amount: ['0.00', [Validators.required, Validators.min(0.01)]],
+      currency: ['', Validators.required],
+      currencies: ['', Validators.required],
+      startDate: [this.getCurrentDateString(), Validators.required],
+      endDate: [this.getCurrentDateString(), Validators.required]
+    });
+
+    this.currencies$ = this.userFacade.currencies$;
+
+    this.categories$ = this.categoryFacade.expenseCategories$;
+
+    this.isLoading$ = this.budgetFacade.isLoading$;
+
+    this.userFacade.loadUserCurrencies();
+
+    this.categoryFacade.loadCategories();
+  }
+
+  private getCurrentDateString(): string {
+    return new Date().toISOString().substring(0, 10);
+  }
 }
