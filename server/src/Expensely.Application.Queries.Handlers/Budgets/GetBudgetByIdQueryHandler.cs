3a05f0907f22762cec0264db37cb3b1d5ecb3d8b
Milan Jovanović <milannis.995@gmail.com>
2021-04-02 20:07:32 +0200

    feat(server-budgets): Added IGetBudgetByIdQueryProcessor and GetBudgetByIdQueryHandler

---
+++ server/src/Expensely.Application.Queries.Handlers/Budgets/GetBudgetByIdQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Expensely.Application.Contracts.Budgets;
+using Expensely.Application.Queries.Budgets;
+using Expensely.Application.Queries.Processors.Budgets;
+using Expensely.Common.Abstractions.Messaging;
+using Expensely.Common.Primitives.Maybe;
+
+namespace Expensely.Application.Queries.Handlers.Budgets
+{
+    /// <summary>
+    /// Represents the <see cref="GetBudgetByIdQuery"/> handler.
+    /// </summary>
+    public sealed class GetBudgetByIdQueryHandler : IQueryHandler<GetBudgetByIdQuery, Maybe<BudgetResponse>>
+    {
+        private readonly IGetBudgetByIdQueryProcessor _processor;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GetBudgetByIdQueryHandler"/> class.
+        /// </summary>
+        /// <param name="processor">The get budget by identifier query processor.</param>
+        public GetBudgetByIdQueryHandler(IGetBudgetByIdQueryProcessor processor) => _processor = processor;
+
+        /// <inheritdoc />
+        public async Task<Maybe<BudgetResponse>> Handle(GetBudgetByIdQuery request, CancellationToken cancellationToken) =>
+            await _processor.Process(request, cancellationToken);
+    }
+}
---
+++ server/src/Expensely.Application.Queries.Processors/Budgets/IGetBudgetByIdQueryProcessor.cs
@@ -0,0 +1,14 @@
+﻿using Expensely.Application.Contracts.Budgets;
+using Expensely.Application.Queries.Budgets;
+using Expensely.Application.Queries.Processors.Abstractions;
+using Expensely.Common.Primitives.Maybe;
+
+namespace Expensely.Application.Queries.Processors.Budgets
+{
+    /// <summary>
+    /// Represents the <see cref="GetBudgetByIdQuery"/> processor interface.
+    /// </summary>
+    public interface IGetBudgetByIdQueryProcessor : IQueryProcessor<GetBudgetByIdQuery, Maybe<BudgetResponse>>
+    {
+    }
+}
