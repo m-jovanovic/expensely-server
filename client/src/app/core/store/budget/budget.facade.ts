fd28f8387ea08024e3bc386735d1e0bb9715e541
Milan Jovanović <milannis.995@gmail.com>
2021-04-02 20:52:23 +0200

    feat(client-budgets): Updated BudgetFacade with budget selector and method for getting budget by id

--- client/src/app/core/store/budget/budget.facade.ts
+++ client/src/app/core/store/budget/budget.facade.ts
@@ -3,13 +3,17 @@ import { Select, Store } from '@ngxs/store';
 import { Observable } from 'rxjs';
 
 import { AuthenticationFacade } from '../authentication/authentication.facade';
-import { CreateBudget } from './budget.actions';
+import { CreateBudget, GetBudget } from './budget.actions';
 import { BudgetSelectors } from './budget.selectors';
+import { BudgetResponse } from '../../contracts/budgets/budget-response';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BudgetFacade {
+  @Select(BudgetSelectors.getBudget)
+  budget$: Observable<BudgetResponse>;
+
   @Select(BudgetSelectors.getIsLoading)
   isLoading$: Observable<boolean>;
 
@@ -17,6 +21,10 @@ export class BudgetFacade {
   error$: Observable<boolean>;
 
   constructor(private store: Store, private authenticationFacade: AuthenticationFacade) {}
+
+  getBudget(budgetId: string): Observable<any> {
+    return this.store.dispatch(new GetBudget(budgetId));
+  }
 
   createBudget(name: string, amount: number, currency: number, categories: number[], startDate: Date, endDate: Date): Observable<any> {
     return this.store.dispatch(new CreateBudget(this.authenticationFacade.userId, name, amount, currency, categories, startDate, endDate));
--- client/src/app/core/store/budget/budget.selectors.ts
+++ client/src/app/core/store/budget/budget.selectors.ts
@@ -2,8 +2,14 @@ import { Selector } from '@ngxs/store';
 
 import { BudgetStateModel } from './budget-state.model';
 import { BudgetState } from './budget.state';
+import { BudgetResponse } from '../../contracts/budgets/budget-response';
 
 export class BudgetSelectors {
+  @Selector([BudgetState])
+  static getBudget(state: BudgetStateModel): BudgetResponse {
+    return state.budget;
+  }
+
   @Selector([BudgetState])
   static getIsLoading(state: BudgetStateModel): boolean {
     return state.isLoading;
