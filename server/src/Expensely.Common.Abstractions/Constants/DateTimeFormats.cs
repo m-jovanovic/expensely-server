bcc1dea3e1a59028ba8cb9fed3f3fc1a4704efbb
Milan Jovanović <milannis.995@gmail.com>
2021-04-02 19:56:38 +0200

    feat(server-common): Added DateTimeExtensions and implemented helper method for formatting a date for display purposes

--- server/src/Expensely.Common.Abstractions/Constants/DateTimeFormats.cs
+++ server/src/Expensely.Common.Abstractions/Constants/DateTimeFormats.cs
@@ -11,6 +11,11 @@
         public const string Date = "yyyy-MM-dd";
 
         /// <summary>
+        /// The display date format.
+        /// </summary>
+        public const string DisplayDate = "dd/MM/yyyy";
+
+        /// <summary>
         /// The date and time with millisecond format.
         /// </summary>
         public const string DateTimeWithMilliseconds = "yyyy-MM-dd HH:mm:ss.fffffff";
---
+++ server/src/Expensely.Common.Abstractions/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using Expensely.Common.Abstractions.Constants;
+
+namespace Expensely.Common.Abstractions.Extensions
+{
+    /// <summary>
+    /// Contains extension methods for the <see cref="DateTime"/> struct.
+    /// </summary>
+    public static class DateTimeExtensions
+    {
+        /// <summary>
+        /// Converts the value of the current <see cref="DateTime"/> object to a display date string representation.
+        /// </summary>
+        /// <param name="dateTime">The date time object.</param>
+        /// <returns>The display date string representation of the <see cref="DateTime"/> object.</returns>
+        public static string ToDisplayDate(this DateTime dateTime) =>
+            dateTime.ToString(DateTimeFormats.DisplayDate, CultureInfo.InvariantCulture);
+    }
+}
