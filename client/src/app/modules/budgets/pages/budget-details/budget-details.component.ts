97bb512036ce3c89271f1435c037188d18e81d09
Milan Jovanović <milannis.995@gmail.com>
2021-04-08 17:25:31 +0200

    feat(client-budgets): Implemented initial version of BudgetDetailsComponent

--- client/src/app/modules/budgets/pages/budget-details/budget-details.component.ts
+++ client/src/app/modules/budgets/pages/budget-details/budget-details.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { TranslocoService } from '@ngneat/transloco';
+
+import { BudgetFacade, BudgetResponse, RouterService } from '@expensely/core';
+import { ConfirmationDialogService } from '@expensely/shared/services';
 
 @Component({
   selector: 'exp-budget-details',
@@ -6,7 +12,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./budget-details.component.scss']
 })
 export class BudgetDetailsComponent implements OnInit {
-  constructor() {}
+  budget$: Observable<BudgetResponse>;
+  isLoading$: Observable<boolean>;
 
-  ngOnInit(): void {}
+  constructor(
+    private route: ActivatedRoute,
+    private budgetFacade: BudgetFacade,
+    private routerService: RouterService,
+    private confirmationDialogService: ConfirmationDialogService,
+    private translationService: TranslocoService
+  ) {}
+
+  ngOnInit(): void {
+    this.budget$ = this.budgetFacade.budget$;
+
+    this.isLoading$ = this.budgetFacade.isLoading$;
+
+    const budgetId = this.route.snapshot.paramMap.get('id');
+
+    this.budgetFacade.getBudget(budgetId);
+  }
+
+  deleteBudget(budgetId: string): void {
+    this.confirmationDialogService.open({
+      message: this.translationService.translate('budgets.delete.dialog.message'),
+      cancelButtonText: this.translationService.translate('budgets.delete.dialog.cancelButtonText'),
+      confirmButtonText: this.translationService.translate('budgets.delete.dialog.confirmButtonText')
+    });
+
+    this.confirmationDialogService.afterClosed().subscribe((confirmed) => {
+      if (!confirmed) {
+        return;
+      }
+
+      this.budgetFacade.deleteBudget(budgetId).subscribe(() => this.routerService.navigate(['/budgets']));
+    });
+  }
+
+  async return(): Promise<boolean> {
+    return await this.routerService.navigate(['/budgets']);
+  }
+
+  async updateBudget(budgetId: string): Promise<boolean> {
+    return await this.routerService.navigate(['budgets', budgetId, 'update']);
+  }
 }
