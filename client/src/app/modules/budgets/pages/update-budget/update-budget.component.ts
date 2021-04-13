ebc719c25837bdb654d5ff38ef3a98fd5e661bb2
Milan Jovanović <milannis.995@gmail.com>
2021-04-13 23:42:14 +0200

    feat(client-budgets): Implemented using the new SelectBudgetCategoryComponent on update page page

--- client/src/app/modules/budgets/pages/update-budget/update-budget.component.ts
+++ client/src/app/modules/budgets/pages/update-budget/update-budget.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { BehaviorSubject, combineLatest, Observable, Subscription } from 'rxjs';
-import { filter, finalize, map, take, tap } from 'rxjs/operators';
+import { combineLatest, Observable } from 'rxjs';
+import { filter, finalize, map, tap } from 'rxjs/operators';
 import { TranslocoService } from '@ngneat/transloco';
 
 import {
@@ -24,15 +24,13 @@ import { NotificationSettings } from '@expensely/shared/constants';
   templateUrl: './update-budget.component.html',
   styleUrls: ['./update-budget.component.scss']
 })
-export class UpdateBudgetComponent implements OnInit, OnDestroy {
+export class UpdateBudgetComponent implements OnInit {
   private requestSent = false;
-  private subscription: Subscription;
-  private selectedCategoriesSubject = new BehaviorSubject<CategoryResponse[]>([]);
+  selectedCategoryIds: number[] = [];
   budget$: Observable<BudgetResponse>;
   updateBudgetForm: FormGroup;
   currencies$: Observable<UserCurrencyResponse[]>;
   categories$: Observable<CategoryResponse[]>;
-  selectedCategories$: Observable<CategoryResponse[]>;
   isLoading$: Observable<boolean>;
   submitted = false;
 
@@ -53,7 +51,6 @@ export class UpdateBudgetComponent implements OnInit, OnDestroy {
       name: ['', [Validators.required, Validators.maxLength(100)]],
       amount: ['0.00', [Validators.required, Validators.min(0.01)]],
       currency: ['', Validators.required],
-      category: [''],
       startDate: ['', [Validators.required, DateRangeValidators.startDateBeforeEndDate]],
       endDate: ['', [Validators.required, DateRangeValidators.endDateAfterStartDate]]
     });
@@ -66,35 +63,21 @@ export class UpdateBudgetComponent implements OnInit, OnDestroy {
           name: budget.name,
           amount: budget.amount,
           currency: budget.currency,
-          category: '',
           startDate: budget.startDate.substring(0, 10),
           endDate: budget.endDate.substring(0, 10)
         });
+
+        this.selectedCategoryIds = budget.categories;
       })
     );
 
-    this.selectedCategories$ = this.selectedCategoriesSubject.asObservable();
-
-    this.categories$ = combineLatest([this.categoryFacade.expenseCategories$, this.selectedCategories$]).pipe(
-      map(([categories, selectedCategories]) => {
-        return categories.filter((category) => !selectedCategories.includes(category));
-      })
-    );
+    this.categories$ = this.categoryFacade.expenseCategories$;
 
     this.currencies$ = this.userFacade.currencies$;
 
     this.isLoading$ = combineLatest([this.budgetFacade.isLoading$, this.userFacade.isLoading$, this.categoryFacade.isLoading$]).pipe(
       map(([budgetIsLoading, userIsLoading, categoryIsLoading]) => budgetIsLoading || userIsLoading || categoryIsLoading)
     );
-
-    this.subscription = combineLatest([this.budget$, this.categoryFacade.expenseCategories$, this.isLoading$])
-      .pipe(
-        filter(([, , isLoading]) => !isLoading),
-        tap(([budget, categories]) => {
-          this.selectedCategoriesSubject.next(categories.filter((category) => budget.categories.includes(category.id)));
-        })
-      )
-      .subscribe();
 
     const budgetId = this.route.snapshot.paramMap.get('id');
 
@@ -103,13 +86,7 @@ export class UpdateBudgetComponent implements OnInit, OnDestroy {
     this.categoryFacade.loadCategories();
   }
 
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
-
-    this.selectedCategoriesSubject.complete();
-  }
-
-  async onSubmit(): Promise<void> {
+  onSubmit(): Promise<void> {
     if (this.requestSent) {
       return;
     }
@@ -131,7 +108,7 @@ export class UpdateBudgetComponent implements OnInit, OnDestroy {
         this.updateBudgetForm.value.name,
         this.updateBudgetForm.value.amount,
         this.updateBudgetForm.value.currency,
-        await this.getSelectedCategoryIds(),
+        this.selectedCategoryIds,
         this.updateBudgetForm.value.startDate,
         this.updateBudgetForm.value.endDate
       )
@@ -152,22 +129,8 @@ export class UpdateBudgetComponent implements OnInit, OnDestroy {
     return await this.routerService.navigate(['budgets', this.budgetFacade.budgetId]);
   }
 
-  addCategory(categoryId: number): void {
-    this.categories$.pipe(take(1)).subscribe((categories: CategoryResponse[]) => {
-      const category = categories.find((category) => category.id === categoryId);
-
-      this.selectedCategories$.pipe(take(1)).subscribe((selectedCategories) => {
-        this.selectedCategoriesSubject.next([...selectedCategories, category]);
-      });
-    });
-
-    this.updateBudgetForm.get('category').setValue('');
-  }
-
-  removeCategory(categoryToRemove: CategoryResponse): void {
-    this.selectedCategories$.pipe(take(1)).subscribe((selectedCategories) => {
-      this.selectedCategoriesSubject.next([...selectedCategories.filter((category) => category != categoryToRemove)]);
-    });
+  setSelectedCategories(selectedCategories: number[]): void {
+    this.selectedCategoryIds = selectedCategories;
   }
 
   private handleUpdateBudgetError(errorResponse: ApiErrorResponse): void {
@@ -178,14 +141,5 @@ export class UpdateBudgetComponent implements OnInit, OnDestroy {
         NotificationSettings.defaultTimeout
       );
     }
-  }
-
-  private async getSelectedCategoryIds(): Promise<number[]> {
-    return await this.selectedCategories$
-      .pipe(
-        take(1),
-        map((selectedCategories: CategoryResponse[]) => selectedCategories.map((c) => c.id))
-      )
-      .toPromise();
   }
 }
