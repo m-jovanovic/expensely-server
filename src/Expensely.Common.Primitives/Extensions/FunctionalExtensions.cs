e7d96f02174454e1529fda991d4f65110ca35524
Milan Jovanović <milannis.995@gmail.com>
2021-05-08 20:09:10 +0200

    feat(common): Created FunctionalExtensions.Map method to simplify controller actions

---
+++ src/Expensely.Common.Primitives/Extensions/FunctionalExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Expensely.Common.Primitives.Extensions
+{
+    /// <summary>
+    /// Contains functional extension methods.
+    /// </summary>
+    public static class FunctionalExtensions
+    {
+        /// <summary>
+        /// Maps the result of the task based on the specified mapping function and return it.
+        /// </summary>
+        /// <typeparam name="TIn">The result type.</typeparam>
+        /// <typeparam name="TOut">The output result type.</typeparam>
+        /// <param name="task">The task.</param>
+        /// <param name="func">The mapping function.</param>
+        /// <returns>The mapped value.</returns>
+        public static async Task<TOut> Map<TIn, TOut>(this Task<TIn> task, Func<TIn, TOut> func) => func(await task);
+    }
+}
